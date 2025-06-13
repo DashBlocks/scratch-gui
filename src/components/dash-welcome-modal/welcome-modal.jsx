00a78134ca1ff1b3ccda9c1f75c6f4de48ffb058
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-06-13 11:02:22 +0500

    Remake realization of Welcome modal: Add modal component

---
+++ src/components/dash-welcome-modal/welcome-modal.jsx
@@ -0,0 +1,44 @@
+import {defineMessages, FormattedMessage, intlShape, injectIntl} from 'react-intl';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Box from '../box/box.jsx';
+import Modal from '../../containers/modal.jsx';
+import classNames from 'classnames';
+
+import styles from './welcome-modal.css';
+
+const WelcomeModalComponent = props => (
+    <Modal
+        className={styles.modalContent}
+        onRequestClose={props.onClose}
+        contentLabel="Welcome to Dash!"
+        id="welcomeModal"
+    >
+        <Box className={styles.body}>
+            <p className={styles.helpText}>
+                Hello, <b>welcome to the Dash!</b>
+                <br>
+                <br>
+                <i>Dash</i> is a <i>TurboWarp</i> and <i>PenguinMod</i> mod with <b>new features</b>.
+                <br>
+                Don't wait, <b>start creating right now!</b>
+                <br>
+                <br>
+                <b>It's recommended to switch language to English (if you didn't already)</b> because some texts aren't translated or translated wrong.
+            </p>
+            <Box className={styles.buttonRow}>
+                <button
+                    className={styles.okButton}
+                    onClick={props.onClose}
+                >Close</button>
+            </Box>
+        </Box>
+    </Modal>
+);
+
+WelcomeModalComponent.propTypes = {
+    intl: intlShape,
+    onClose: PropTypes.func.isRequired
+};
+
+export default injectIntl(UsernameModalComponent);

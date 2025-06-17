3cf59add76f149e43c14461f4c409cb843fdb754
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-06-17 09:54:36 +0500

    Update welcome-modal.jsx

--- src/components/dash-welcome-modal/welcome-modal.jsx
+++ src/components/dash-welcome-modal/welcome-modal.jsx
@@ -5,6 +5,7 @@ import Box from '../box/box.jsx';
 import Modal from '../../containers/modal.jsx';
 import classNames from 'classnames';
 
+import poster from './Poster for Dash with Dashy.png';
 import styles from './welcome-modal.css';
 
 const WelcomeModalComponent = props => (
@@ -27,7 +28,7 @@ const WelcomeModalComponent = props => (
                 <b>It's recommended to switch language to English (if you didn't already)</b> because some texts aren't translated or translated wrong.
                 <br />
                 <br />
-                <img src="./Poster for Dash with Dashy.png" />
+                <img src={poster} />
             </p>
             <Box className={styles.buttonRow}>
                 <button

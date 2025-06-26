583786808b16a344c6b900cebe09d4db4a4160be
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-06-26 14:05:49 +0500

    Update library-item.jsx

--- src/components/library-item/library-item.jsx
+++ src/components/library-item/library-item.jsx
@@ -218,7 +218,7 @@ class LibraryItemComponent extends React.PureComponent {
                                                             <br />
                                                         )}
                                                     </React.Fragment>
-                                                )}
+                                                ))}
                                             </React.Fragment>
                                         )}
                                     </div>

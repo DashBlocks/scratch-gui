5fb705f0f76217ab42fe3584fc096d9bd9fa0b02
DBDev-git <dambul2809@mail.ru>
2025-08-21 16:42:32 +0700

    New block: operator_nums_in_range

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -731,6 +731,19 @@ const operators = function (isInitialSetup, isStage, targetId, colors) {
                 </shadow>
               </value>
             </block>
+            ${blockSeparator}
+            <block type="operator_nums_in_range">
+              <value name="FROM">
+                <shadow type="math_number">
+                  <field name="NUM">1</field>
+                </shadow>
+              </value>
+              <value name="TO">
+                <shadow type="math_number">
+                  <field name="NUM">10</field>
+                </shadow>
+              </value>
+            </block>
             <block type="operator_in_range">
               <value name="NUM">
                 <shadow type="math_number">

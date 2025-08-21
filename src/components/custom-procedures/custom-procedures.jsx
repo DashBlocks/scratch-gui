ffcd99a2f9ea560fd9785f87d7861a4bd567475f
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-08-21 14:27:07 +0500

    Fixes

--- src/components/custom-procedures/custom-procedures.jsx
+++ src/components/custom-procedures/custom-procedures.jsx
@@ -22,152 +22,148 @@ const messages = defineMessages({
     }
 });
 
-const CustomProcedures = props => (
+const CustomProcedures = (props) => {
+  const ScratchBlocks = LazyScratchBlocks.get();
+  const themeObj = props.getCustomExtensionColors();
+  return (
     <Modal
-        className={styles.modalContent}
-        contentLabel={props.intl.formatMessage(messages.myblockModalTitle)}
-        onRequestClose={props.onCancel}
-        id="customProceduresModal"
+      className={styles.modalContent}
+      contentLabel={props.intl.formatMessage(messages.myblockModalTitle)}
+      onRequestClose={props.onCancel}
+      id="customProceduresModal"
     >
-        <Box
-            className={styles.workspace}
-            componentRef={props.componentRef}
-        />
-        <Box className={styles.body}>
-            <div className={styles.optionsRow}>
-                <div
-                    className={styles.optionCard}
-                    role="button"
-                    tabIndex="0"
-                    onClick={props.onAddTextNumber}
-                >
-                    <img
-                        className={styles.optionIcon}
-                        src={textInputIcon}
-                        draggable={false}
-                    />
-                    <div className={styles.optionTitle}>
-                        <FormattedMessage
-                            defaultMessage="Add an input"
-                            description="Label for button to add a number/text input"
-                            id="gui.customProcedures.addAnInputNumberText"
-                        />
-                    </div>
-                    <div className={styles.optionDescription}>
-                        <FormattedMessage
-                            defaultMessage="number or text"
-                            description="Description of the number/text input type"
-                            id="gui.customProcedures.numberTextType"
-                        />
-                    </div>
-                </div>
-                <div
-                    className={styles.optionCard}
-                    role="button"
-                    tabIndex="0"
-                    onClick={props.onAddBoolean}
-                >
-                    <img
-                        className={styles.optionIcon}
-                        src={booleanInputIcon}
-                        draggable={false}
-                    />
-                    <div className={styles.optionTitle}>
-                        <FormattedMessage
-                            defaultMessage="Add an input"
-                            description="Label for button to add a boolean input"
-                            id="gui.customProcedures.addAnInputBoolean"
-                        />
-                    </div>
-                    <div className={styles.optionDescription}>
-                        <FormattedMessage
-                            defaultMessage="boolean"
-                            description="Description of the boolean input type"
-                            id="gui.customProcedures.booleanType"
-                        />
-                    </div>
-                </div>
-                <div
-                    className={styles.optionCard}
-                    role="button"
-                    tabIndex="0"
-                    onClick={props.onAddLabel}
-                >
-                    <img
-                        className={styles.optionIcon}
-                        src={labelIcon}
-                        draggable={false}
-                    />
-                    <div className={styles.optionTitle}>
-                        <FormattedMessage
-                            defaultMessage="Add a label"
-                            description="Label for button to add a label"
-                            id="gui.customProcedures.addALabel"
-                        />
-                    </div>
-                </div>
+      <Box className={styles.workspace} componentRef={props.componentRef} />
+      <Box className={styles.body}>
+        <div className={styles.optionsRow}>
+          <div
+            className={styles.optionCard}
+            role="button"
+            tabIndex="0"
+            onClick={props.onAddTextNumber}
+          >
+            <img
+              className={styles.optionIcon}
+              src={textInputIcon}
+              draggable={false}
+            />
+            <div className={styles.optionTitle}>
+              <FormattedMessage
+                defaultMessage="Add an input"
+                description="Label for button to add a number/text input"
+                id="gui.customProcedures.addAnInputNumberText"
+              />
             </div>
-            <div className={styles.colorRow}>
-                {((ScratchBlocks) => Object.entries(ScratchBlocks.Colours)
-                    .filter((v) => Object.values(ScratchBlocks.Categories).includes(v[0]))
-                    .map((v, index) => (
-                        <div
-                            key={index}
-                            style={{ backgroundColor: v[1].primary }}
-                            className={styles.colorOption}
-                            role="button"
-                            draggable={false}
-                            onClick={() => props.setColor(blockColors[v[0]].primary)}
-                        />
-                    ))
-                )(LazyScratchBlocks.get())}
-                <input
-                    style={{ backgroundColor: props.theme.getCustomExtensionColors().primary(props.color) }}
-                    type="color"
-                    value={props.color}
-                    className={classNames(styles.colorOption, styles.colorPicker)}
-                    onChange={(e) => props.setColor(e.target.value)}
+            <div className={styles.optionDescription}>
+              <FormattedMessage
+                defaultMessage="number or text"
+                description="Description of the number/text input type"
+                id="gui.customProcedures.numberTextType"
+              />
+            </div>
+          </div>
+          <div
+            className={styles.optionCard}
+            role="button"
+            tabIndex="0"
+            onClick={props.onAddBoolean}
+          >
+            <img
+              className={styles.optionIcon}
+              src={booleanInputIcon}
+              draggable={false}
+            />
+            <div className={styles.optionTitle}>
+              <FormattedMessage
+                defaultMessage="Add an input"
+                description="Label for button to add a boolean input"
+                id="gui.customProcedures.addAnInputBoolean"
+              />
+            </div>
+            <div className={styles.optionDescription}>
+              <FormattedMessage
+                defaultMessage="boolean"
+                description="Description of the boolean input type"
+                id="gui.customProcedures.booleanType"
+              />
+            </div>
+          </div>
+          <div
+            className={styles.optionCard}
+            role="button"
+            tabIndex="0"
+            onClick={props.onAddLabel}
+          >
+            <img
+              className={styles.optionIcon}
+              src={labelIcon}
+              draggable={false}
+            />
+            <div className={styles.optionTitle}>
+              <FormattedMessage
+                defaultMessage="Add a label"
+                description="Label for button to add a label"
+                id="gui.customProcedures.addALabel"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.colorRow}>
+          {Object.entries(ScratchBlocks.Colours)
+              .filter((v) => Object.values(ScratchBlocks.Categories).includes(v[0]))
+              .map((v, index) => (
+                <div
+                  key={index}
+                  style={{ backgroundColor: v[1].primary }}
+                  className={styles.colorOption}
+                  role="button"
+                  draggable={false}
+                  onClick={() => props.setColor(blockColors[v[0]].primary)}
                 />
-            </div>
-            <div className={styles.checkboxRow}>
-                <label>
-                    <input
-                        checked={props.warp}
-                        type="checkbox"
-                        onChange={props.onToggleWarp}
-                    />
-                    <FormattedMessage
-                        defaultMessage="Run without screen refresh"
-                        description="Label for checkbox to run without screen refresh"
-                        id="gui.customProcedures.runWithoutScreenRefresh"
-                    />
-                </label>
-            </div>
-            <Box className={styles.buttonRow}>
-                <button
-                    className={styles.cancelButton}
-                    onClick={props.onCancel}
-                >
-                    <FormattedMessage
-                        defaultMessage="Cancel"
-                        description="Label for button to cancel custom procedure edits"
-                        id="gui.customProcedures.cancel"
-                    />
-                </button>
-                <button
-                    className={styles.okButton}
-                    onClick={props.onOk}
-                >
-                    <FormattedMessage
-                        defaultMessage="OK"
-                        description="Label for button to save new custom procedure"
-                        id="gui.customProcedures.ok"
-                    />
-                </button>
-            </Box>
+              ))}
+          <input
+            style={{
+              backgroundColor: Object.keys(themeObj).length === 0 ? props.color : themeObj.primary(props.color)
+            }}
+            type="color"
+            value={props.color}
+            className={classNames(styles.colorOption, styles.colorPicker)}
+            onChange={(e) => props.setColor(e.target.value)}
+          />
+        </div>
+        <div className={styles.checkboxRow}>
+          <label>
+            <input
+              checked={props.warp}
+              type="checkbox"
+              onChange={props.onToggleWarp}
+            />
+            <FormattedMessage
+              defaultMessage="Run without screen refresh"
+              description="Label for checkbox to run without screen refresh"
+              id="gui.customProcedures.runWithoutScreenRefresh"
+            />
+          </label>
+        </div>
+        <Box className={styles.buttonRow}>
+          <button className={styles.cancelButton} onClick={props.onCancel}>
+            <FormattedMessage
+              defaultMessage="Cancel"
+              description="Label for button to cancel custom procedure edits"
+              id="gui.customProcedures.cancel"
+            />
+          </button>
+          <button className={styles.okButton} onClick={props.onOk}>
+            <FormattedMessage
+              defaultMessage="OK"
+              description="Label for button to save new custom procedure"
+              id="gui.customProcedures.ok"
+            />
+          </button>
         </Box>
+      </Box>
     </Modal>
-);
+  );
+};
             
 CustomProcedures.propTypes = {
     componentRef: PropTypes.func.isRequired,
@@ -182,7 +178,7 @@ CustomProcedures.propTypes = {
 };
 
 const mapStateToProps = state => ({
-    theme: state.scratchGui.theme.theme
+    getCustomExtensionColors: state.scratchGui.theme.theme.getCustomExtensionColors
 });
 
 export default injectIntl(connect(

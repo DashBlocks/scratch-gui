73b3c8adf19c611da7e29c505ea3693a9cdd6ad5
Damir <dambul2809@mail.ru>
2025-10-15 11:55:41 +0700

    Update render-interface.jsx

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -157,15 +157,6 @@ const RenderWelcomeModal = () => {
     );
 }
 
-const checkForTests = () => {
-    if (window.location.href == "https://dashblocks.github.io/scratch-gui") {
-        const url = new URL(window.location.href);
-        const params = url.searchParams;
-        params.append('enabletests', '');
-    }
-}
-
-checkForTests();
 runAddons();
 
 const Footer = () => (

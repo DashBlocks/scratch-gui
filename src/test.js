1d011d0bfd84b1e573b7324aee5bb691a0e6b4fe
DBDev-IT-test <damir.b.dbdev@gmail.com>
2025-11-19 07:55:42 +0000

    Build

--- src/test.js
+++
@@ -1,9 +0,0 @@
-const checkForTests = () => {
-    if (window.location.href == "https://dashblocks.github.io/scratch-gui") {
-        const url = new URL(window.location.href);
-        const params = url.searchParams;
-        params.append('enabletests', '');
-    }
-}
-
-export default checkForTests;

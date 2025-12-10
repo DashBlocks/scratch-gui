eb64bc275be649998bb2d7d20b6ba3de60afeb3f
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-12-10 13:37:56 +0500

    Update themePersistance.js

--- src/lib/themes/themePersistance.js
+++ src/lib/themes/themePersistance.js
@@ -65,7 +65,8 @@ const detectTheme = () => {
         return new Theme(
             parsed.accent || systemPreferences.accent,
             parsed.gui || systemPreferences.gui,
-            parsed.blocks || systemPreferences.blocks
+            parsed.blocks || systemPreferences.blocks,
+            parsed.wallpaper || null
         );
     } catch (e) {
         // ignore
@@ -90,6 +91,9 @@ const persistTheme = theme => {
     // custom blocks are managed by addon at runtime, don't save here
     if (theme.blocks !== systemPreferences.blocks && theme.blocks !== BLOCKS_CUSTOM) {
         nonDefaultSettings.blocks = theme.blocks;
+    }
+    if (theme.wallpaper.url !== null) {
+        nonDefaultSettings.wallpaper = theme.wallpaper;
     }
 
     if (Object.keys(nonDefaultSettings).length === 0) {

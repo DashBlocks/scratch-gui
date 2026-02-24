6dc14c7883f16222213e9ea00297a46c168b34a8
Damir <dambul2809@mail.ru>
2026-02-24 05:00:34 +0000

    ...

--- src/lib/tw-state-manager-hoc.jsx
+++ src/lib/tw-state-manager-hoc.jsx
@@ -60,7 +60,7 @@ const getLocalStorage = key => {
 };
 
 const readHashProjectId = () => {
-    const match = window.location.hash.match(/#(s?\d+)/);
+    const match = location.hash.match(/#(s?\d+)/);
     return match === null ? null : match[1];
 };
 

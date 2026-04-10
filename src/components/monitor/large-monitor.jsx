059e7b8bf631b96bcc042deba7a224d1932ce4a9
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-04-10 10:34:01 +0500

    Use isCustomType method from Cast

--- src/components/monitor/large-monitor.jsx
+++ src/components/monitor/large-monitor.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './monitor.css';
 import DOMElementRenderer from '../../containers/dom-element-renderer.jsx';
+import Cast from 'scratch-vm/src/utils/cast.js';
 
 const LargeMonitor = ({categoryColor, value}) => (
     <div className={styles.largeMonitor}>
@@ -12,7 +13,7 @@ const LargeMonitor = ({categoryColor, value}) => (
                 color: categoryColor.text
             }}
         >
-            {typeof value?.customId === 'string' && typeof value?.toMonitorContent === 'function'
+            {Cast.isCustomType(value) && typeof value?.toMonitorContent === 'function'
                 ? (<DOMElementRenderer domElement={value.toMonitorContent()} />)
                 : String(value)}
         </div>

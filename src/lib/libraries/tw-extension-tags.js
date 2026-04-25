4aac752f4fe7dff04c6ae8173ad1c16e17db03bc
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-25 14:48:24 +0300

    Update tw-extension-tags.js

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -3,7 +3,8 @@ import {APP_NAME} from '../brand';
 // Because there are all brand names, it is unnecessary for them to be translatable.
 export default [
     {tag: 'scratch', intlLabel: 'Scratch'},
-    {tag: 'dash', intlLabel: APP_NAME},
+    {tag: 'tsmod', intlLabel: APP_NAME},
+    {tag: 'dash', intlLabel: 'Dash'},
     {tag: 'tw', intlLabel: 'TurboWarp'},
     {tag: 'other', intlLabel: 'Other'}
-];
+]

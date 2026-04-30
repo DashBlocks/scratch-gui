4a84324b32ac84b8b377d93a89aa73abdf8562f1
Damir <dambul2809@mail.ru>
2026-04-30 04:07:06 +0000

    lazy-menu-bar.jsx: Remove more unused stuff

--- src/components/menu-bar/lazy-menu-bar.jsx
+++ src/components/menu-bar/lazy-menu-bar.jsx
@@ -1,10 +1,9 @@
 import classNames from 'classnames';
 import {connect} from 'react-redux';
 import {compose} from 'redux';
-import {defineMessages, FormattedMessage, injectIntl, intlShape} from 'react-intl';
+import {FormattedMessage, injectIntl, intlShape} from 'react-intl';
 import PropTypes from 'prop-types';
 import bindAll from 'lodash.bindall';
-import bowser from 'bowser';
 import React from 'react';
 
 import VM from 'scratch-vm';
@@ -13,74 +12,31 @@ import Box from '../box/box.jsx';
 import Button from '../button/button.jsx';
 import {ComingSoonTooltip} from '../coming-soon/coming-soon.jsx';
 import Divider from '../divider/divider.jsx';
-import MenuBarMenu from './menu-bar-menu.jsx';
-import MenuLabel from './tw-menu-label.jsx';
-import {MenuItem, MenuSection} from '../menu/menu.jsx';
+import {MenuItem} from '../menu/menu.jsx';
 import MenuBarHOC from '../../containers/menu-bar-hoc.jsx';
 import SettingsMenu from './lazy-settings-menu.jsx';
 import AccountNav from '../../containers/account-nav.jsx';
 
-import TWSaveStatus from './tw-save-status.jsx';
 import TWNews from './tw-news.jsx';
 import {isNewYearMode} from '../../components/dash-new-year-mode/new-year-mode.jsx';
-import getSession from '../../lib/session';
 
-import {openTipsLibrary, openSettingsModal, openRestorePointModal} from '../../reducers/modals';
-import {setPlayer} from '../../reducers/mode';
 import {setSession} from '../../reducers/dash';
 import {
-    isTimeTravel220022BC,
-    isTimeTravel1920,
-    isTimeTravel1990,
-    isTimeTravel2020,
-    isTimeTravelNow,
-    setTimeTravel
-} from '../../reducers/time-travel';
-import {
-    openAboutMenu,
-    closeAboutMenu,
-    aboutMenuOpen,
     openAccountMenu,
     closeAccountMenu,
     accountMenuOpen,
-    openLoginMenu,
-    closeLoginMenu,
-    loginMenuOpen,
-    openModeMenu,
-    closeModeMenu,
-    modeMenuOpen,
     settingsMenuOpen,
     openSettingsMenu,
     closeSettingsMenu
 } from '../../reducers/menus';
-import {setFileHandle} from '../../reducers/tw.js';
-
-import collectMetadata from '../../lib/collect-metadata';
 
 import styles from './menu-bar.css';
 
-import helpIcon from '../../lib/assets/icon--tutorials.svg';
 import mystuffIcon from './icon--mystuff.png';
-import profileIcon from './icon--profile.png';
-import remixIcon from './icon--remix.svg';
-import dropdownCaret from './dropdown-caret.svg';
-import aboutIcon from './icon--about.svg';
-import fileIcon from './icon--file.svg';
-import addonsIcon from './addons.svg';
-
-import errorIcon from './tw-error.svg';
-import advancedIcon from './tw-advanced.svg';
 import dashLogo from './dash.png';
 import dashNewYearLogo from './dash-new-year.png'
 
-import ninetiesLogo from './nineties_logo.svg';
-import catLogo from './cat_logo.svg';
-import prehistoricLogo from './prehistoric-logo.svg';
-import oldtimeyLogo from './oldtimey-logo.svg';
-
-import sharedMessages from '../../lib/shared-messages';
-
-import isScratchDesktop, {notScratchDesktop} from '../../lib/isScratchDesktop.js';
+import isScratchDesktop from '../../lib/isScratchDesktop.js';
 import {APP_NAME} from '../../lib/brand.js';
 
 const MenuBarItemTooltip = ({
@@ -137,19 +93,6 @@ MenuItemTooltip.propTypes = {
     isRtl: PropTypes.bool
 };
 
-const AboutButton = props => (
-    <Button
-        className={classNames(styles.menuBarItem, styles.hoverable)}
-        iconClassName={styles.aboutIcon}
-        iconSrc={aboutIcon}
-        onClick={props.onClick}
-    />
-);
-
-AboutButton.propTypes = {
-    onClick: PropTypes.func.isRequired
-};
-
 // Unlike <MenuItem href="">, this uses an actual <a>
 const MenuItemLink = props => (
     <a
@@ -171,43 +114,18 @@ class LazyMenuBar extends React.Component {
     constructor (props) {
         super(props);
         bindAll(this, [
-            'handleClickDesktopSettings',
-            'handleClickLogOut',
-            'handleSetMode',
+            'handleClickLogOut'
         ]);
     }
-    handleClickDesktopSettings () {
-        this.props.onClickDesktopSettings();
-        this.props.onRequestCloseSettings();
-    }
-    handleClickLogOut () {
-        // log out
-    }
-    handleSetMode (mode) {
-        this.props.onSetTimeTravelMode(mode);
-        this.props.onRequestCloseMode();
-    }
-    buildAboutMenu (onClickAbout) {
-        if (!onClickAbout) {
-            return null;
-        }
-        if (typeof onClickAbout === 'function') {
-            return <AboutButton onClick={onClickAbout} />;
-        }
-        // Otherwise, assume it's an array of objects
-        // TODO: implement
-        return null;
+    async handleClickLogOut () {
+        const response = await fetch('https://dashblocks-server.vercel.app/auth/logout', {credentials: 'include'});
+        const data = await response.json();
+        if (!data.ok)
+            return alert('Sign out failed');
+        this.props.setSession(null);
+        window.location.reload();
     }
     render () {
-        const newProjectMessage = (
-            <FormattedMessage
-                defaultMessage="New"
-                description="Menu bar item for creating a new project"
-                id="gui.menuBar.new"
-            />
-        );
-        // Show the About button only if we have a handler for it (like in the desktop app)
-        const aboutButton = this.buildAboutMenu(this.props.onClickAbout);
         const menuBar = (
             <Box
                 className={classNames(
@@ -357,192 +275,51 @@ class LazyMenuBar extends React.Component {
 }
 
 LazyMenuBar.propTypes = {
-    enableSeeInside: PropTypes.bool,
-    onClickSeeInside: PropTypes.func,
-    aboutMenuOpen: PropTypes.bool,
     accountMenuOpen: PropTypes.bool,
-    authorId: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-    authorThumbnailUrl: PropTypes.string,
-    authorUsername: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-    autoUpdateProject: PropTypes.func,
     canChangeLanguage: PropTypes.bool,
     canChangeTheme: PropTypes.bool,
-    canCreateCopy: PropTypes.bool,
-    canCreateNew: PropTypes.bool,
-    canEditTitle: PropTypes.bool,
-    canManageFiles: PropTypes.bool,
-    canRemix: PropTypes.bool,
-    canSave: PropTypes.bool,
-    canShare: PropTypes.bool,
     className: PropTypes.string,
-    errors: PropTypes.arrayOf(PropTypes.shape({
-        sprite: PropTypes.string,
-        error: PropTypes.string,
-        id: PropTypes.number
-    })),
-    errorsMenuOpen: PropTypes.bool,
-    onClickErrors: PropTypes.func,
-    onRequestCloseErrors: PropTypes.func,
-    confirmReadyToReplaceProject: PropTypes.func,
     currentLocale: PropTypes.string.isRequired,
-    enableCommunity: PropTypes.bool,
-    fileMenuOpen: PropTypes.bool,
-    handleSaveProject: PropTypes.func,
     intl: intlShape,
-    isPlayerOnly: PropTypes.bool,
     isRtl: PropTypes.bool,
-    isShared: PropTypes.bool,
-    isShowingProject: PropTypes.bool,
-    isTotallyNormal: PropTypes.bool,
-    isUpdating: PropTypes.bool,
     locale: PropTypes.string.isRequired,
-    loginMenuOpen: PropTypes.bool,
-    mode1920: PropTypes.bool,
-    mode1990: PropTypes.bool,
-    mode2020: PropTypes.bool,
-    mode220022BC: PropTypes.bool,
-    modeMenuOpen: PropTypes.bool,
-    modeNow: PropTypes.bool,
-    onClickAbout: PropTypes.oneOfType([
-        PropTypes.func, // button mode: call this callback when the About button is clicked
-        PropTypes.arrayOf( // menu mode: list of items in the About menu
-            PropTypes.shape({
-                title: PropTypes.string, // text for the menu item
-                onClick: PropTypes.func // call this callback when the menu item is clicked
-            })
-        )
-    ]),
     onClickAccount: PropTypes.func,
     onClickAddonSettings: PropTypes.func,
-    onClickDesktopSettings: PropTypes.func,
-    onClickPackager: PropTypes.func,
-    onClickRestorePoints: PropTypes.func,
-    onClickEdit: PropTypes.func,
-    onClickFile: PropTypes.func,
-    onClickLogin: PropTypes.func,
-    onClickMode: PropTypes.func,
-    onClickNew: PropTypes.func,
-    onClickNewWindow: PropTypes.func,
-    onClickRemix: PropTypes.func,
-    onClickSave: PropTypes.func,
-    onClickSaveAsCopy: PropTypes.func,
     onClickSettings: PropTypes.func,
     onClickSettingsModal: PropTypes.func,
     onLogOut: PropTypes.func,
-    onOpenRegistration: PropTypes.func,
-    onOpenTipLibrary: PropTypes.func,
-    onProjectTelemetryEvent: PropTypes.func,
-    onRequestCloseAbout: PropTypes.func,
     onRequestCloseAccount: PropTypes.func,
-    onRequestCloseEdit: PropTypes.func,
-    onRequestCloseFile: PropTypes.func,
-    onRequestCloseLogin: PropTypes.func,
-    onRequestCloseMode: PropTypes.func,
     onRequestCloseSettings: PropTypes.func,
-    onRequestOpenAbout: PropTypes.func,
-    onSeeCommunity: PropTypes.func,
-    onSetTimeTravelMode: PropTypes.func,
-    onShare: PropTypes.func,
-    onStartSelectingFileUpload: PropTypes.func,
-    onToggleLoginOpen: PropTypes.func,
-    projectId: PropTypes.string,
-    projectTitle: PropTypes.string,
-    renderLogin: PropTypes.func,
     sessionExists: PropTypes.bool,
     settingsMenuOpen: PropTypes.bool,
-    shouldSaveBeforeTransition: PropTypes.func,
-    showSaveFilePicker: PropTypes.func,
-    showComingSoon: PropTypes.bool,
     setSession: PropTypes.func,
-    username: PropTypes.string,
-    userOwnsProject: PropTypes.bool,
     vm: PropTypes.instanceOf(VM).isRequired
 };
 
 LazyMenuBar.defaultProps = {
-    onShare: () => {},
     canChangeLanguage: true,
-    canChangeTheme: true,
-    canCreateCopy: false,
-    canCreateNew: false,
-    canEditTitle: false,
-    canManageFiles: false,
-    canRemix: false,
-    canSave: false,
-    canShare: false,
-    enableCommunity: false,
-    isShared: false,
-    showComingSoon: false
+    canChangeTheme: true
 };
 
-const mapStateToProps = (state, ownProps) => {
-    const loadingState = state.scratchGui.projectState.loadingState;
+const mapStateToProps = state => {
     const session = state.scratchGui.dash.session;
     return {
-        authorUsername: state.scratchGui.tw.author.username,
-        authorId: state.scratchGui.tw.author.userId,
-        authorThumbnailUrl: state.scratchGui.tw.author.thumbnail,
-        projectId: state.scratchGui.projectState.projectId,
-        aboutMenuOpen: aboutMenuOpen(state),
         accountMenuOpen: accountMenuOpen(state),
         currentLocale: state.locales.locale,
-        fileMenuOpen: fileMenuOpen(state),
-        errors: state.scratchGui.tw.compileErrors,
-        errorsMenuOpen: errorsMenuOpen(state),
-        isPlayerOnly: state.scratchGui.mode.isPlayerOnly,
         isRtl: state.locales.isRtl,
-        isUpdating: getIsUpdating(loadingState),
-        isShowingProject: getIsShowingProject(loadingState),
         locale: state.locales.locale,
-        loginMenuOpen: loginMenuOpen(state),
-        modeMenuOpen: modeMenuOpen(state),
-        projectTitle: state.scratchGui.projectTitle,
         sessionExists: state.scratchGui.dash.session !== null,
         settingsMenuOpen: settingsMenuOpen(state),
         session: session || null,
-        userOwnsProject: ownProps.authorUsername && session &&
-            (ownProps.authorUsername === session.username),
-        vm: state.scratchGui.vm,
-        mode220022BC: isTimeTravel220022BC(state),
-        mode1920: isTimeTravel1920(state),
-        mode1990: isTimeTravel1990(state),
-        mode2020: isTimeTravel2020(state),
-        modeNow: isTimeTravelNow(state)
+        vm: state.scratchGui.vm
     };
 };
 
 const mapDispatchToProps = dispatch => ({
-    onClickSeeInside: () => dispatch(setPlayer(false)),
-    autoUpdateProject: () => dispatch(autoUpdateProject()),
-    onOpenTipLibrary: () => dispatch(openTipsLibrary()),
     onClickAccount: () => dispatch(openAccountMenu()),
     onRequestCloseAccount: () => dispatch(closeAccountMenu()),
-    onClickFile: () => dispatch(openFileMenu()),
-    onRequestCloseFile: () => dispatch(closeFileMenu()),
-    onClickErrors: () => dispatch(openErrorsMenu()),
-    onRequestCloseErrors: () => dispatch(closeErrorsMenu()),
-    onClickLogin: () => dispatch(openLoginMenu()),
-    onRequestCloseLogin: () => dispatch(closeLoginMenu()),
-    onClickMode: () => dispatch(openModeMenu()),
-    onRequestCloseMode: () => dispatch(closeModeMenu()),
-    onRequestOpenAbout: () => dispatch(openAboutMenu()),
-    onRequestCloseAbout: () => dispatch(closeAboutMenu()),
-    onClickRestorePoints: () => dispatch(openRestorePointModal()),
     onClickSettings: () => dispatch(openSettingsMenu()),
-    onClickSettingsModal: () => {
-        dispatch(closeEditMenu());
-        dispatch(openSettingsModal());
-    },
     onRequestCloseSettings: () => dispatch(closeSettingsMenu()),
-    onClickNew: needSave => {
-        dispatch(requestNewProject(needSave));
-        dispatch(setFileHandle(null));
-    },
-    onClickRemix: () => dispatch(remixProject()),
-    onClickSave: () => dispatch(manualUpdateProject()),
-    onClickSaveAsCopy: () => dispatch(saveProjectAsCopy()),
-    onSeeCommunity: () => dispatch(setPlayer(true)),
-    onSetTimeTravelMode: mode => dispatch(setTimeTravel(mode)),
     setSession: session => dispatch(setSession(session))
 });
 

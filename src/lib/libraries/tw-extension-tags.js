f591d4f8162ecb1927b4f1e6940c51a1a9eed5ea
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-05-27 16:32:57 +0500

    tw-extension-tags.js: ⬆ Update

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -1,6 +1,7 @@
 import messages from './tag-messages.js';
-
+import tagGroupMessages from './dash-tag-group-messages.js';
 export default [
+    {isGroup: true, intlLabel: tagGroupMessages.platform},
     {tag: 'scratch', intlLabel: 'Scratch'}, // Because is a brand name, it's unnecessary for to be translatable.
     {tag: 'dash', intlLabel: messages.dash},
     {tag: 'tw', intlLabel: 'TurboWarp'}, // Because is a brand name, it's unnecessary for to be translatable.

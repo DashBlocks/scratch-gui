5ce5929a7ea22f137bf6c308d81d10853b29d28b
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-06-22 14:40:26 +0500

    sprite-costume-library-item.jsx: 🗑 Remove unneeded stuff

--- src/components/library-item/sprite-costume-library-item.jsx
+++ src/components/library-item/sprite-costume-library-item.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Box from '../box/box.jsx';
-// import PlayButton from '../../containers/play-button.jsx';
 import styles from './library-item.css';
 import classNames from 'classnames';
 
@@ -23,15 +22,13 @@ class SpriteCostumeLibraryItem extends React.PureComponent {
                 onClick={this.props.onClick}
                 onFocus={this.props.onFocus}
                 onKeyPress={this.props.onKeyPress}
-                onMouseEnter={this.props.showPlayButton ? null : this.props.onMouseEnter}
-                onMouseLeave={this.props.showPlayButton ? null : this.props.onMouseLeave}
+                onMouseEnter={this.props.onMouseEnter}
+                onMouseLeave={this.props.onMouseLeave}
             >
                 {/* Layers of wrapping is to prevent layout thrashing on animation */}
                 <Box className={styles.libraryItemImageContainerWrapper}>
                     <Box
                         className={styles.libraryItemImageContainer}
-                        onMouseEnter={this.props.showPlayButton ? this.props.onMouseEnter : null}
-                        onMouseLeave={this.props.showPlayButton ? this.props.onMouseLeave : null}
                     >
                         <img
                             className={styles.libraryItemImage}
@@ -59,13 +56,6 @@ class SpriteCostumeLibraryItem extends React.PureComponent {
                         </>
                     )}
                 </div>
-                {/*this.props.showPlayButton ? (
-                    <PlayButton
-                        isPlaying={this.props.isPlaying}
-                        onPlay={this.props.onPlay}
-                        onStop={this.props.onStop}
-                    />
-                ) :*/ null}
 
                 {this.props.favoriteButton}
             </Box>
@@ -79,7 +69,6 @@ SpriteCostumeLibraryItem.propTypes = {
     favoriteButton: PropTypes.node,
     hidden: PropTypes.bool,
     iconURL: PropTypes.string,
-    isPlaying: PropTypes.bool,
     name: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.node
@@ -89,14 +78,7 @@ SpriteCostumeLibraryItem.propTypes = {
     onFocus: PropTypes.func.isRequired,
     onKeyPress: PropTypes.func.isRequired,
     onMouseEnter: PropTypes.func.isRequired,
-    onMouseLeave: PropTypes.func.isRequired,
-    onPlay: PropTypes.func.isRequired,
-    onStop: PropTypes.func.isRequired,
-    showPlayButton: PropTypes.bool
-};
-
-SpriteCostumeLibraryItem.defaultProps = {
-    showPlayButton: false
+    onMouseLeave: PropTypes.func.isRequired
 };
 
 export default SpriteCostumeLibraryItem;

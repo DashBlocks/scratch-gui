d3c212fb90d223be90af701274db2d29adda3b33
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-10 22:17:05 +0500

    dash-web-libraries.js: ⬆ Update

--- src/lib/libraries/dash-web-libraries.js
+++ src/lib/libraries/dash-web-libraries.js
@@ -1,7 +1,28 @@
 const webLibraries = {
     dash: {
-        getAssetURL: path => `https://raw.githubusercontent.com/DashBlocks/assets/refs/heads/main${path}`
+        getAssetURL: path => `https://raw.githubusercontent.com/DashBlocks/assets/refs/heads/main${path}`,
+        handleAsset: (item, vm, handleUpload, handleNewAsset) => {
+            try {
+                const res = await fetch(`https://raw.githubusercontent.com/DashBlocks/assets/refs/heads/main${item.src.path}`);
+                const blob = await res.blob();
+
+                const fileType = blob.type;
+                const buffer = await blob.arrayBuffer();
+
+                handleUpload(buffer, fileType, vm, vmAssets => {
+                    vmAssets.forEach((asset, i) => {
+                        asset.name = `${item.name}${i ? i + 1 : ''}`;
+                    });
+                    handleNewAsset(vmAssets);
+                });
+            } catch (_) {
+                // ignore
+            }
+        }
     }
 };
 
 export const getAssetURL = (library, path) => webLibraries[library]?.getAssetURL?.(path);
+
+export const handleAsset = (item, vm, handleUpload, handleNewAsset) =>
+    webLibraries[item.src?.library]?.handleAsset?.(item, vm, handleUpload, handleNewAsset);

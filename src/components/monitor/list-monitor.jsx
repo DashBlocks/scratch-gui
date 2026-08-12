507821fd3773a038bd321f0b178d19981ed47bc4
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-08-12 13:16:08 +0500

    list-monitor.jsx: ⬆ Update

--- src/components/monitor/list-monitor.jsx
+++ src/components/monitor/list-monitor.jsx
@@ -50,7 +50,7 @@ const ListMonitor = ({draggable, label, width, height, value, path, onNavigateTo
                     description="Length label on list monitors. DO NOT translate {length} (with brackets)."
                     id="gui.monitor.listMonitor.listLength"
                     values={{
-                        length: Array.isArray(value) ? value.length : Object.keys(value || {}).length
+                        length: value.length
                     }}
                 />
             </div>

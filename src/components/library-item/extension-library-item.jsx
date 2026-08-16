e5c44bb01848da568dd13b1cbaf1ecb87cf533c6
Damir <dambul2809@mail.ru>
2026-08-16 04:44:48 +0000

    extension-library-item.jsx: Fix

--- src/components/library-item/extension-library-item.jsx
+++ src/components/library-item/extension-library-item.jsx
@@ -2,7 +2,6 @@ import {FormattedMessage} from 'react-intl';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Box from '../box/box.jsx';
 import styles from './library-item.css';
 import extensionItemStyles from './extension-library-item.css';
 import classNames from 'classnames';
@@ -13,6 +12,15 @@ import internetConnectionIconURL from './internet-connection.svg';
 /* eslint-disable react/prefer-stateless-function */
 class ExtensionLibraryItem extends React.PureComponent {
     render () {
+        const hasMetadata = (
+            this.props.bluetoothRequired ||
+            this.props.internetConnectionRequired ||
+            this.props.collaborator ||
+            (this.props.credits && this.props.credits.length > 0) ||
+            this.props.docsURI ||
+            this.props.samples
+        );
+
         return (
             <div
                 className={classNames(
@@ -42,6 +50,7 @@ class ExtensionLibraryItem extends React.PureComponent {
                         src={this.props.iconURL}
                     />
                 </div>
+
                 {this.props.insetIconURL ? (
                     <div className={styles.libraryItemInsetImageContainer}>
                         <img
@@ -51,157 +60,143 @@ class ExtensionLibraryItem extends React.PureComponent {
                         />
                     </div>
                 ) : null}
+
                 <div className={styles.featuredText}>
                     <span>{this.props.name}</span>
                     <br />
                     <span className={styles.featuredDescription}>{this.props.description}</span>
                 </div>
 
-                {(
-                    this.props.bluetoothRequired ||
-                  this.props.internetConnectionRequired ||
-                  this.props.collaborator ||
-                  (this.props.credits && this.props.credits.length > 0) ||
-                  this.props.docsURI ||
-                  this.props.samples
-                ) ? (
+                {hasMetadata ? (
                     <div
-                            className={classNames(
-                                styles.libraryItemMetadata,
-                                extensionItemStyles.libraryItemMetadata
-                            )}
-                        >
-                            {this.props.bluetoothRequired || this.props.internetConnectionRequired ? (
+                        className={classNames(
+                            styles.libraryItemMetadata,
+                            extensionItemStyles.libraryItemMetadata
+                        )}
+                    >
+                        {this.props.bluetoothRequired || this.props.internetConnectionRequired ? (
                             <div className={styles.libraryItemMetadataSection}>
+                                <div>
                                     <div>
-                                    <div>
-                                            <FormattedMessage
+                                        <FormattedMessage
                                             defaultMessage="Requires"
                                             description="Label for extension hardware requirements"
                                             id="gui.extensionLibrary.requires"
                                         />
-                                        </div>
-                                    <div
-                                            className={styles.libraryItemMetadataDetail}
-                                        >
-                                            {this.props.bluetoothRequired ? (
+                                    </div>
+                                    <div className={styles.libraryItemMetadataDetail}>
+                                        {this.props.bluetoothRequired ? (
                                             <img
-                                                    src={bluetoothIconURL}
-                                                    draggable={false}
-                                                />
-                                            ) : null}
-                                            {this.props.internetConnectionRequired ? (
+                                                src={bluetoothIconURL}
+                                                draggable={false}
+                                            />
+                                        ) : null}
+                                        {this.props.internetConnectionRequired ? (
                                             <img
-                                                    src={internetConnectionIconURL}
-                                                    draggable={false}
-                                                />
-                                            ) : null}
-                                        </div>
+                                                src={internetConnectionIconURL}
+                                                draggable={false}
+                                            />
+                                        ) : null}
+                                    </div>
                                 </div>
-                                </div>
-                            ) : null}
-                            {this.props.collaborator ? (
+                            </div>
+                        ) : null}
+
+                        {this.props.collaborator ? (
                             <div className={styles.libraryItemMetadataSection}>
+                                <div>
                                     <div>
-                                    <div>
-                                            <FormattedMessage
+                                        <FormattedMessage
                                             defaultMessage="Collaboration with"
                                             description="Label for extension collaboration"
                                             id="gui.extensionLibrary.collaboration"
                                         />
-                                        </div>
-                                    <div
-                                            className={styles.libraryItemMetadataDetail}
-                                        >
-                                            {this.props.collaborator}
-                                        </div>
+                                    </div>
+                                    <div className={styles.libraryItemMetadataDetail}>
+                                        {this.props.collaborator}
+                                    </div>
                                 </div>
-                                </div>
-                            ) : null}
-                            {this.props.credits && this.props.credits.length > 0 ? (
+                            </div>
+                        ) : null}
+
+                        {this.props.credits && this.props.credits.length > 0 ? (
                             <div className={styles.libraryItemMetadataSection}>
+                                <div>
                                     <div>
-                                    <div>
-                                            <FormattedMessage
+                                        <FormattedMessage
                                             defaultMessage="Created by"
                                             description="Appears in the extension list. Followed by a list of names."
                                             id="tw.createdBy"
                                         />
-                                        </div>
-                                    <div
-                                            className={styles.libraryItemMetadataDetail}
-                                        >
-                                            {this.props.credits.map((credit, index) => (
+                                    </div>
+                                    <div className={styles.libraryItemMetadataDetail}>
+                                        {this.props.credits.map((credit, index) => (
                                             <React.Fragment key={index}>
-                                                    {credit}
-                                                    {index !== this.props.credits.length - 1 && (
-                                                        ', '
-                                                    )}
-                                                </React.Fragment>
-                                            ))}
-                                        </div>
+                                                {credit}
+                                                {index !== this.props.credits.length - 1 && ', '}
+                                            </React.Fragment>
+                                        ))}
+                                    </div>
                                 </div>
-                                </div>
-                            ) : null}
-                            {this.props.docsURI || this.props.samples ? (
+                            </div>
+                        ) : null}
+
+                        {this.props.docsURI || this.props.samples ? (
                             <div className={styles.libraryItemMetadataSection}>
+                                <div>
                                     <div>
-                                    <div>
-                                            <FormattedMessage
+                                        <FormattedMessage
                                             defaultMessage="Resources"
                                             description="Label for extension resources"
                                             id="dash.extensionLibrary.resources"
                                         />
-                                        </div>
-                                    <div
-                                            className={styles.libraryItemMetadataDetail}
-                                        >
-                                            {this.props.docsURI && (
-                                        <a
-                                                    href={this.props.docsURI}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    <FormattedMessage
-                                                defaultMessage="Documentation"
-                                                        // eslint-disable-next-line max-len
-                                                description="Appears in the extension list. Links to additional extension documentation."
-                                                id="tw.documentation"
-                                            />
-                                                </a>
-                                            )}
-                                            {this.props.samples && (
-                                        <React.Fragment>
-                                                    {this.props.docsURI && (
-                                            <br />
-                                                    )}
-                                                    {this.props.samples.map((sample, index) => (
-                                                <React.Fragment key={index}>
-                                                            <a
-                                                                href={sample.href}
-                                                                target="_blank"
-                                                                rel="noreferrer"
-                                                            >
-                                                                <FormattedMessage
-                                                                    defaultMessage="Sample project"
-                                                                    // eslint-disable-next-line max-len
-                                                                    description="Appears in the extension list. Links to a sample project for an extension."
-                                                                    id="tw.sample"
-                                                                />
-                                                            </a>
-                                                            {index !== this.props.samples.length - 1 && (
-                                                                <br />
-                                                            )}
-                                                        </React.Fragment>
-                                                    ))}
-                                                </React.Fragment>
-                                            )}
-                                        </div>
+                                    </div>
+                                    <div className={styles.libraryItemMetadataDetail}>
+                                        {this.props.docsURI && (
+                                            <a
+                                                href={this.props.docsURI}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                <FormattedMessage
+                                                    defaultMessage="Documentation"
+                                                    // eslint-disable-next-line max-len
+                                                    description="Appears in the extension list. Links to additional extension documentation."
+                                                    id="tw.documentation"
+                                                />
+                                            </a>
+                                        )}
+
+                                        {this.props.samples && (
+                                            <React.Fragment>
+                                                {this.props.docsURI && <br />}
+                                                {this.props.samples.map((sample, index) => (
+                                                    <React.Fragment key={index}>
+                                                        <a
+                                                            href={sample.href}
+                                                            target="_blank"
+                                                            rel="noreferrer"
+                                                        >
+                                                            <FormattedMessage
+                                                                defaultMessage="Sample project"
+                                                                // eslint-disable-next-line max-len
+                                                                description="Appears in the extension list. Links to a sample project for an extension."
+                                                                id="tw.sample"
+                                                            />
+                                                        </a>
+                                                        {index !== this.props.samples.length - 1 && (
+                                                            <br />
+                                                        )}
+                                                    </React.Fragment>
+                                                ))}
+                                            </React.Fragment>
+                                        )}
+                                    </div>
                                 </div>
-                                </div>
-                            ) : null}
-                        </div>
-                    ) : null}
+                            </div>
+                        ) : null}
+                    </div>
+                ) : null}
 
                 {this.props.favoriteButton}
             </div>
@@ -209,7 +204,6 @@ class ExtensionLibraryItem extends React.PureComponent {
     }
 }
 /* eslint-enable react/prefer-stateless-function */
-
 
 ExtensionLibraryItem.propTypes = {
     bluetoothRequired: PropTypes.bool,
@@ -219,7 +213,6 @@ ExtensionLibraryItem.propTypes = {
         PropTypes.node
     ]),
     disabled: PropTypes.bool,
-    extensionId: PropTypes.string,
     favoriteButton: PropTypes.node,
     hidden: PropTypes.bool,
     iconURL: PropTypes.string,
